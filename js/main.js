b33e777a20a1d9f63961e5e52732901703401ab3
yukesh dhakal <102406963+YukeshDhakal@users.noreply.github.com>
2024-12-04 16:06:03 +1100

    Update main.js

--- js/main.js
+++ js/main.js
@@ -97,6 +97,26 @@
 		});
 
 	};
+	$('.owl-carousel').owlCarousel({
+  loop: true,
+  margin: 10,
+  responsiveClass: true,
+  responsive: {
+    0: {
+      items: 1,
+      nav: true,
+    },
+    600: {
+      items: 1,
+      nav: true,
+    },
+    1000: {
+      items: 1,
+      nav: true,
+    }
+  }
+});
+
 	carousel();
 
 	$('nav .dropdown').hover(function(){
